01cd42464b8c66ba82c8cc3dcb5d931dc2d7d7a4
DuduBertolDev <dudubertoldev@gmail.com>
2025-10-29 10:07:12 -0300

    Initial Commit

---
+++ TrainingGraphs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrainingGraphs
+//
+//  Created by Eduardo Bertol on 28/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrainingGraphs/TrainingGraphsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingGraphsApp.swift
+//  TrainingGraphs
+//
+//  Created by Eduardo Bertol on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TrainingGraphsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
